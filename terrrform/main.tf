2039c90fe193964b3133e41b709270dd388ed051
kishore-athmnathan <161807501+kishore-athmnathan@users.noreply.github.com>
2024-03-05 09:42:49 +0530

    Add files via upload

---
+++ terrrform/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+module "ec2_instance" {
+    source = "./module/ec2_instance"
+    ami_value = "ami-0440d3b780d96b29d"
+    instance_type_value = "t2.micro"
+}
---
+++ terrrform/module/ec2_instance/variable.tf
@@ -0,0 +1,8 @@
+variable "ami_value" {
+    description = "ami for example ec2"
+  
+}
+
+variable "instance_type_value" {
+    description = "instance type for example ec2"
+}
